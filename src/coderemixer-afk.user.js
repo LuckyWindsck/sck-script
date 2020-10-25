30a40f6b10d1aa84be207e951481eb2cbe799a16
LuckyWind_sck <luckywindsck@gmail.com>
2020-10-25 20:00:02 +0900

    refactor: use vue.js & remove usage of DOM API

--- src/coderemixer-afk.user.js
+++ src/coderemixer-afk.user.js
@@ -5,20 +5,33 @@
 // @description  Update slide / Auto answering
 // @author       LuckyWind_sck
 // @match        https://coderemixer.com/
+// @require      https://cdn.jsdelivr.net/npm/vue@2.6.12
 // @require      https://momentjs.com/downloads/moment-with-locales.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
+/* global Vue, moment */
 (() => {
-  'use strict';
-  const author = Array.from(document.querySelectorAll("a"))
-      .find(e => e.href.includes("twitter")).href.split("/").pop();
-  const daysNotUpdating = moment().diff(moment(document.querySelector('[itemprop="datePublished"]').innerText), "days");
-  const message = `${author} 已经小鸡带弓【${document.title}】${daysNotUpdating} 天了`;
-  const section = document.createElement("section").appendChild(
-      document.createElement("p").appendChild(
-          document.createTextNode(message)).parentNode).parentNode;
-  document.querySelector(".h1").insertAdjacentElement("afterend", section);
-  section.id = "last-update";
+  const AFK = Vue.extend({
+    data() {
+      return {
+        author: document.querySelector('a[href*=twitter]').href.split('/').pop(),
+        title: document.title,
+        lastUpdate: document.querySelector('[itemprop="datePublished"]').innerText,
+      };
+    },
+    computed: {
+      daysNoUpdate() {
+        return moment().diff(moment(this.lastUpdate), 'days');
+      },
+      message() {
+        return `${this.author} 已经小鸡带弓【${this.title}】${this.daysNoUpdate} 天了`;
+      },
+    },
+    template: '<section id="last-update"><p>{{ message }}</p></section>',
+  });
+
+  const archives = document.querySelector('.h1');
+  archives.insertAdjacentElement('afterend', new AFK().$mount().$el);
 })();
